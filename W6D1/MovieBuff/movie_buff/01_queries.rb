7e5b443f4aacb093e7ec28f57bbd0b2459383e25
Zohreh Sadeghi <zsadeghi@uw.edu>
2019-09-23 23:06:36 -0700

    first set of movie buff problems

--- W6D1/MovieBuff/movie_buff/01_queries.rb
+++ W6D1/MovieBuff/movie_buff/01_queries.rb
@@ -6,7 +6,7 @@ def it_was_ok
   # We can use ranges (a..b) inside a where method.
   #
   # Find the id, title, and score of all movies with scores between 2 and 3
-
+  Movie.select(:id, :title, :score).where(score: 2..3)
 end
 
 def harrison_ford
@@ -20,6 +20,12 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
+
+  Movie
+    .select(:id, :title)
+    .joins(:actors)
+    .where(actors: { name: 'Harrison Ford' })
+    .where.not( castings: { ord: 1 } )
 
 end
 
@@ -38,6 +44,13 @@ def biggest_cast
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
 
+  Movie
+    .select(:id, :title)
+    .joins(:castings)
+    .group(:title, :id)
+    .order('COUNT(*) DESC')
+    .limit(3)
+
 end
 
 def directed_by_one_of(them)
@@ -52,7 +65,9 @@ def directed_by_one_of(them)
   # Movie.where(yr: years)
   #
   # Find the id and title of all the movies directed by one of 'them'.
-
+  Movie.select(:id, :title)
+    .joins(:director)
+    .where(actors: { name: them })
 end
 
 def movie_names_before_1940
@@ -66,5 +81,9 @@ def movie_names_before_1940
   # improve performace for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
+
+  Movie
+    .where('yr < 1940')
+    .pluck(:title)
 
 end
