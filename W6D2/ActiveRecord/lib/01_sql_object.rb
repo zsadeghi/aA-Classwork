40fadb1db0ff098e8ff2a8b846d86452beed0d59
Zohreh Sadeghi <zsadeghi@uw.edu>
2019-09-24 21:08:18 -0700

    attempt at sql_object building

--- W6D2/ActiveRecord/lib/01_sql_object.rb
+++ W6D2/ActiveRecord/lib/01_sql_object.rb
@@ -5,22 +5,48 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    return @columns if @columns
+    new_cols = DBConnection.excecute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      LIMIT
+        0
+    SQL
+    new_cols.map!(&:to_sym)
+    @columns = new_cols
   end
 
   def self.finalize!
+    self.columns.each do |name|
+      define_methos(name) do
+        self.attributes[name]
+      end
+
+      define_method("#{name}=") do |value|
+        self.attributes[name] = value
+      end
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name || self.name.underscore.pluralize
   end
 
   def self.all
-    # ...
+    results = DBConnection.execite(<<-SQL)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+    SQL
+
+    parse_all(results)
   end
 
   def self.parse_all(results)
@@ -36,7 +62,7 @@ class SQLObject
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
@@ -52,6 +78,6 @@ class SQLObject
   end
 
   def save
-    # ...
+    id.nil? ? insert : update
   end
 end
