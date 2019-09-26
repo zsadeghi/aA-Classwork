af2eb2bd59ce5822e257d1501236e16b41118cd9
Zohreh Sadeghi <zsadeghi@uw.edu>
2019-09-26 02:21:00 -0700

    art work share api phase 2

--- W6D3/art-share-api/config/routes.rb
+++ W6D3/art-share-api/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'artwork', to: 'artwork#create'
+  delete 'artwork/:id', to: 'artwork#destroy'
+  get 'artwork', to: 'artwork#index'
+  get 'artwork/:id', to: 'artwork#show'
+  patch 'artwork/:id', to: 'artwork#update'
+
+  post 'users', to:'users#create'
+  delete 'users/:id', to:'users#destroy'
+  get 'users', to: 'users#index'
+  get 'users/:id', to: 'users#show'
+  patch 'users/:id', to: 'users#update'
 end
