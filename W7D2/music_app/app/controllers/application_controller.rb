bf78df013c0e1abb849d7e50731e15abdcdc8128
Zohré (Zoë)  Sadeghi <zsadeghi@uw.edu>
2019-10-01 23:18:00 -0700

    Update application_controller.rb

--- W7D2/music_app/app/controllers/application_controller.rb
+++ W7D2/music_app/app/controllers/application_controller.rb
@@ -9,7 +9,6 @@ class ApplicationController < ActionController::Base
     def login!(user)
         @current_user = user
         session[:session_token] = @current_user.reset_session_token!
-        #session[:session_token] = user.reset_session_token! (this also works)
     end
 
     def logged_in?
@@ -22,7 +21,6 @@ class ApplicationController < ActionController::Base
     end
 
     def require_user!
-        # redirect_to new_user_url if current_user (require no user)
         redirect_to new_session_url if current_user.nil?
     end
 end
