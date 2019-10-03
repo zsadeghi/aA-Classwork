ead1c6f581e3d1a4d4f56fd34b8534f6b89c5682
Zohreh Sadeghi <zsadeghi@uw.edu>
2019-10-02 18:53:31 -0700

    writing rspec for Users for model and controller

--- W7D3/Goal App/Goal/spec/models/user_spec.rb
+++ W7D3/Goal App/Goal/spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  subject(:user) do
+    User.create!(
+      username: "Kiki",
+      password: "password"
+    )
+
+    describe "password encryption" do
+      it "does not save passwords to the database" do
+        User.create!(username: "Mal_brian", password: "werere")
+        user = User.find_by(username: "Mal_brian")
+        expect(user.password).not_to be("werere")
+      end
+
+      it "encrypts the password using BCrypt" do
+        expect(BCrypt::Password).to recieve(:create)
+        User.new(username: "Mal_brian", password: "werere")
+      end
+    end
+
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username)}
+    it { should have_many(:goals) }
+    it { should have_many(:cheers_given) }
+    it { should have_many(:cheers_received).through(:goals) }
+  end
 end
